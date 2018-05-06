f21c7717155414975cbf92b727b2c74b42d33cd8
fidanzaEPSI <frank.fidanza@epsi.fr>
2018-05-06 19:22:14 +0200

    Removed phinx.yml, dump.sql, updated views

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     protected $table = 'users';
-    protected $fillable = ['name', 'email', 'password'];
+    protected $fillable = ['username', 'email', 'password'];
     protected $hidden = ['password'];
     protected $dates = ['created_at', 'updated_at'];
 }
